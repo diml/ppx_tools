a196e67f45219c1e8b56c2ec2594ae158d8fc551
Jeremie Dimino <jdimino@janestreet.com>
2015-06-25 13:43:33 +0100

    Initialize the lexbuf location in rewriter.ml

--- rewriter.ml
+++ rewriter.ml
@@ -61,16 +61,21 @@ let wrap_open fn file =
     exit 1
 
 let make_lexer source_kind source =
-  match source_kind, source with
-  | `String, _ ->
-    Location.input_name := "//toplevel//";
-    Lexing.from_string source
-  | `Path, "-" ->
-    Location.input_name := "//toplevel//";
-    Lexing.from_channel stdin
-  | `Path, _ ->
-    Location.input_name := source;
-    Lexing.from_channel (wrap_open open_in source)
+  let name, lexbuf =
+    match source_kind, source with
+    | `String, _ ->
+      ("//toplevel//",
+      Lexing.from_string source)
+    | `Path, "-" ->
+      ("//toplevel//",
+       Lexing.from_channel stdin)
+    | `Path, _ ->
+      (source,
+       Lexing.from_channel (wrap_open open_in source))
+  in
+  Location.input_name := name;
+  Location.init lexbuf name;
+  lexbuf
 
 let () =
   Arg.parse args anon_arg usage_msg;
